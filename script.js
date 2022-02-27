9c6d34c07f9f392888d204934d5f043cf93c409e
Slav Konovalov <slav.konovalov@gmail.com>
2022-02-27 23:18:58 +0000

    portfolio init

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-// for (let i = 1; i <= 3; i++) {
-//     document.querySelector(`.page-${i}`).addEventListener('mouseover', function() {
-//         document.querySelector(`.analytics-${i}`).classList.remove('hidden');
-
-//     })
-// }
-
 // Burger Menu Toggle
 
 const brgrBox = document.querySelector(".brgr-box");
 
 brgrBox.addEventListener("click", function () {
-  document.querySelectorAll(".nav-menu li").forEach((o) => {
-    o.classList.toggle("hidden");
-    o.classList.toggle("visible");
-  });
-  brgrBox.classList.toggle("menu-open");
+  if (!brgrBox.classList.contains("menu-open")) {
+    document.querySelectorAll(".nav-menu li").forEach(function (o) {
+      o.classList.remove("hidden");
+      o.classList.add("visible");
+    });
+    brgrBox.classList.add("menu-open");
+  } else if (brgrBox.classList.contains("menu-open")) {
+    document.querySelectorAll(".nav-menu li").forEach(function (o) {
+      o.classList.add("hidden");
+      o.classList.remove("visible");
+    });
+    brgrBox.classList.remove("menu-open");
+  }
 });
 
-// document.querySelector('.page-1').addEventListener('mouseover', function() {
-//     document.querySelector('.analytics-1').classList.remove('hidden');
-//     document.querySelector('.analytics-2').classList.add('hidden');
-//     document.querySelector('.analytics-3').classList.add('hidden');
-// })
+// Sliders
 
-// document.querySelector('.page-2').addEventListener('mouseover', function() {
-//     document.querySelector('.analytics-1').classList.add('hidden');
-//     document.querySelector('.analytics-2').classList.remove('hidden');
-//     document.querySelector('.analytics-3').classList.add('hidden');
-// })
+function sliderHighlights(prj) {
+  prj.forEach(function (b) {
+    b.onclick = function (e) {
+      prj.forEach(function (c) {
+        c.classList[e.target == c ? "add" : "remove"]("highlight");
+      });
+    };
+  });
+}
 
-// document.querySelector('.page-3').addEventListener('mouseover', function() {
-//     document.querySelector('.analytics-1').classList.add('hidden');
-//     document.querySelector('.analytics-2').classList.add('hidden');
-//     document.querySelector('.analytics-3').classList.remove('hidden');
-// })
+sliderHighlights(document.querySelectorAll(".analytics-page .bar"));
+sliderHighlights(document.querySelectorAll(".sports-data-page .bar"));
+sliderHighlights(document.querySelectorAll(".debt-solutions-page .bar"));
