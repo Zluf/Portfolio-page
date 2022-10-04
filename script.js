b578a06f6e390d3a217aa61b1751fc847f73bf91
Slav Konovalov <slav.konovalov@gmail.com>
2022-10-04 16:58:18 +0100

    fix head part1

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
 // Burger Menu Toggle
 
-const brgrBox = document.querySelector(".header__brgr-menu");
+const brgrBox = document.querySelector(".hero__brgr-menu");
 
 brgrBox.addEventListener("click", function () {
   if (!brgrBox.classList.contains("menu-open")) {
     document.querySelectorAll(".nav-menu li").forEach(function (o) {
-      o.classList.remove("hidden");
-      o.classList.add("visible");
+      o.classList.remove("mobile-hidden");
+      o.classList.add("mobile-visible");
     });
     brgrBox.classList.add("menu-open");
   } else if (brgrBox.classList.contains("menu-open")) {
     document.querySelectorAll(".nav-menu li").forEach(function (o) {
-      o.classList.add("hidden");
-      o.classList.remove("visible");
+      o.classList.add("mobile-hidden");
+      o.classList.remove("mobile-visible");
     });
     brgrBox.classList.remove("menu-open");
   }
 });
 
-// Sliders
-
-function sliderHighlights(prj) {
-  prj.forEach(function (b) {
-    b.onclick = function (e) {
-      prj.forEach(function (c) {
-        c.classList[e.target == c ? "add" : "remove"]("highlight");
-      });
-    };
-  });
-}
-
-sliderHighlights(document.querySelectorAll(".analytics-page .bar"));
-sliderHighlights(document.querySelectorAll(".sports-data-page .bar"));
-sliderHighlights(document.querySelectorAll(".debt-solutions-page .bar"));
-
 // Sliders v2.0
 
-// const slideReels = document.querySelectorAll(".slider__reel");
-const slideButtons = document.querySelectorAll(".slider__btn");
+const slideButtons = document.querySelectorAll(".slide-btn");
 
 slideButtons.forEach((btn, i) => {
   btn.addEventListener("click", function (e) {
     const targetedSlideReel =
-      e.target.parentElement.parentElement.children[2].children[0];
+      e.target.parentElement.parentElement.parentElement.children[2]
+        .children[0];
     const amountOfSlides = targetedSlideReel.children.length;
     const btnNum = e.target.className.charAt(e.target.className.length - 1);
     targetedSlideReel.style.transform = `translateX(-${
