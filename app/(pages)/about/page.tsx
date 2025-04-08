e6df30dddf52c309447528a05b9a02efa26ba193
Slav Konovalov <slav.konovalov@gmail.com>
2025-04-08 17:46:22 +0100

    modified link to 2025 CV

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import classes from "./About.module.scss";
+import Image from 'next/image'
+import classes from './About.module.scss'
 
 export default function About() {
   return (
@@ -10,7 +10,7 @@ export default function About() {
         <p>
           My name is Slav Konovalov. I'm a Front-End Developer residing in
           Manchester, UK. My expertise lies in crafting applications and web
-          pages using <strong>React.js</strong> (including extensions{" "}
+          pages using <strong>React.js</strong> (including extensions{' '}
           <strong>Next.js</strong> and <strong>React Native</strong>) with a
           strong focus on both UI design and functionality.
         </p>
@@ -19,7 +19,7 @@ export default function About() {
           My journey into the realm of Front-End Development began in 2021,
           spurred by a desire for a career change. Starting with the
           fundamentals of HTML/CSS, I began crating web pages as practical
-          exercises. Guided by the expertise of a seasoned developer,{" "}
+          exercises. Guided by the expertise of a seasoned developer,{' '}
           <a href="https://tomparson.com/">Tom Parson</a> from Huddle Digital, I
           delved into the principles and standards of Front-End Development.
         </p>
@@ -32,14 +32,17 @@ export default function About() {
         </p>
 
         <p>
-          Presently, I am proud to be a member of the esteemed developer team at{" "}
+          Presently, I am proud to be a member of the esteemed developer team at{' '}
           <a href="https://springact.org">Spring Act</a>, an NGO dedicated to
           supporting victims of domestic violence.
         </p>
 
         <p>
-          For further details, please feel free to explore my resume {""}
-          <a href="https://drive.google.com/file/d/1ips-8N7Tt-9lNKJd_y7_PKe1nhRqbS5d/view?usp=sharing">
+          For further details, please feel free to explore my resume {''}
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1txfBTioVl87Z592CRhtT6syIjzJwTlQ0/view?usp=sharing"
+          >
             here
           </a>
           .
@@ -53,5 +56,5 @@ export default function About() {
         alt="Slav Konovalov Frontend Web Developer logo"
       />
     </section>
-  );
+  )
 }
